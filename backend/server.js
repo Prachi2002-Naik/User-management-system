a193d10125e1544ee576fe568d1731e4ae52e831
Prachi <pn71900@gmail.com>
2026-06-02 13:19:03 +0530

    changes on server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require('./config/db')
-const userRoute = require('./routes/userRutes')
-
 require("dotenv").config();
+
+const connectDB = require("./config/db");
+const userRoute = require("./routes/userRutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-app.use("/api/users", userRoute);
-
 app.get("/", (req, res) => {
   res.send("Server Running...");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/users", userRoute);
 
+// FIRST connect DB properly
 connectDB();
+
+// THEN listen
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
